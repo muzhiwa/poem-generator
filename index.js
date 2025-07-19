aa80a1769c494f3de92497370bb6a915af89698c
muzhiwa <muzhdawafa2008@gmail.com>
2025-07-19 15:22:02 +0430

    integrated AI API, typwriter and updated index.html

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-alert("Welcome to my site"); 
+function displayPoem(response) {
+    new Typewriter('#poem', {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "", 
+    });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+    
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "71260f74c15bct2a0o7b939feaa3813b";
+    let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTMl and seperate ecg line with a <br /> Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and not at the beginning";
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem)
